dad906f42f149a8306f41938d72ed6c7c3c4ee2a
juashyam <kumar.30.shyam@gmail.com>
2025-01-19 21:07:51 +0530

    Add deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
 });
